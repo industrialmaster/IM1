66103dc82822cd1fee4c795c7a725bcf3cfe7d29
Jayan <mail4jayanchinthaka@gmail.com>
2016-10-12 10:36:52 +0530

    Add new button - Jayan

---
+++ app/src/main/java/com/industrialmaster/im/JayanActivity.java
@@ -0,0 +1,13 @@
+package com.industrialmaster.im;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JayanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jayan);
+    }
+}
