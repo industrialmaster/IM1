ead76ef7c7837dc5663df98886b101674c597ec5
sadhee <yasminsamith@gmail.com>
2016-10-12 11:02:51 +0530

    Add new button

--- app/src/main/java/com/industrialmaster/im/PWForget.java
+++ app/src/main/java/com/industrialmaster/im/PWForget.java
@@ -2,12 +2,17 @@ package com.industrialmaster.im;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Sadeera extends AppCompatActivity {
+public class PWForget extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sadeera);
+    }
+
+    public  void updatepw (View v){
+        
     }
 }
